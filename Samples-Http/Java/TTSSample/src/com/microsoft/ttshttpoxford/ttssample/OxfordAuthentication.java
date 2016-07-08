80439b22fb8ce7bef07da227ea96e5e4938e7c35
Ryuichi Tokugami <magcot.com@gmail.com>
2016-07-08 11:48:23 +0900

    Update OxfordAuthentication.java

--- Samples-Http/Java/TTSSample/src/com/microsoft/ttshttpoxford/ttssample/OxfordAuthentication.java
+++ Samples-Http/Java/TTSSample/src/com/microsoft/ttshttpoxford/ttssample/OxfordAuthentication.java
@@ -37,7 +37,6 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.net.URLEncoder;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -160,5 +159,10 @@ public class OxfordAuthentication
         }
 
         return null;
+    }
+    static class Util {
+        public static String getJsonValue(String src, String name) {
+            return src.replaceAll(".*\"" + name + "\":\"(.*?)\".*", "$1");
+        }
     }
 }
