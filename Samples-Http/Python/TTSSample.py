2b59617743163f19d51c40bf1cee12990140a6bc
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-12 11:03:24 -0800

    error trap on response.status

--- Samples-Http/Python/TTSSample.py
+++ Samples-Http/Python/TTSSample.py
@@ -1,6 +1,6 @@
 ###
 #Copyright (c) Microsoft Corporation
-#All rights reserved. 
+#All rights reserved.
 #MIT License
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -35,20 +35,23 @@ print ("Access Token: " + accesstoken)
 
 body = "<speak version='1.0' xml:lang='en-us'><voice xml:lang='en-us' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>This is a demo to call microsoft text to speach service in python.</voice></speak>"
 
-headers = {"Content-type": "application/ssml+xml", 
-			"X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm", 
-			"Authorization": "Bearer " + accesstoken, 
-			"X-Search-AppId": "07D3234E49CE426DAA29772419F436CA", 
-			"X-Search-ClientID": "1ECFAE91408841A480F00935DC390960", 
+headers = {"Content-type": "application/ssml+xml",
+			"X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm",
+			"Authorization": "Bearer " + accesstoken,
+			"X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
+			"X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
 			"User-Agent": "TTSForPython"}
-			
+
 #Connect to server to synthesize the wave
 print ("\nConnect to server to synthesize the wave")
 conn = http.client.HTTPSConnection("speech.platform.bing.com")
 conn.request("POST", "/synthesize", body, headers)
 response = conn.getresponse()
-print(response.status, response.reason)
 
-data = response.read()
+if response.status == 200:
+    data = response.read()
+    print("The synthesized wave length: %d" %(len(data)))
+else:
+    print("Response Error: ",response.status,response.reason)
+
 conn.close()
-print("The synthesized wave length: %d" %(len(data)))
