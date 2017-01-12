7102e6166045fea7009e858a7d220cffaaffb50a
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-12 10:59:49 -0800

    documenting the response object

---
+++ Samples-Http/Python/documentResponseObject.py
@@ -0,0 +1,18 @@
+"""
+response.read - HTTPResponse
+response.readable - HTTPResponse
+response.readall - HTTPResponse
+response.readinto - HTTPResponse
+response.readline - HTTPResponse
+response.readlines - HTTPResponse
+response.reason = <class 'str'> i.e. "Access Denied" or "OK"
+response.seek - HTTPResponse
+response.seekable - HTTPResponse
+response.status = <class 'int'> i.e 401, 200
+response.tell - HTTPResponse
+response.truncate - HTTPResponse
+response.version = <class 'int'> i.e. 11
+response.will_close = <class 'bool'> i.e. False
+response.writable - HTTPResponse
+response.writelines - HTTPResponse
+"""
