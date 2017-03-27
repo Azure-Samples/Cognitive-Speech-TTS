8915941318ada839be56cb379be0c8211de54b47
yangguo <v-guoya@microsoft.com>
2017-03-27 11:02:27 +0800

    Modify Azure SDK to support streaming

--- ClientSDK/azure-c-shared-utility/adapters/httpapi_winhttp.c
+++ ClientSDK/azure-c-shared-utility/adapters/httpapi_winhttp.c
@@ -704,6 +704,411 @@ HTTPAPI_RESULT HTTPAPI_ExecuteRequest(HTTP_HANDLE handle, HTTPAPI_REQUEST_TYPE r
     return result;
 }
 
+HTTPAPI_RESULT HTTPAPI_Request(HTTP_HANDLE handle, HTTPAPI_REQUEST_TYPE requestType, const char* relativePath,
+	HTTP_HEADERS_HANDLE httpHeadersHandle, const unsigned char* content,
+	size_t contentLength, REQUESTHANDLE httpRequestHandle)
+{
+	HTTPAPI_RESULT result;
+	if (g_HTTPAPIState != HTTPAPI_INITIALIZED)
+	{
+		LogError("g_HTTPAPIState not HTTPAPI_INITIALIZED");
+		result = HTTPAPI_NOT_INIT;
+	}
+	else
+	{
+		HTTP_HANDLE_DATA* handleData = (HTTP_HANDLE_DATA*)handle;
+
+		if ((handleData == NULL) ||
+			(relativePath == NULL) ||
+			(httpHeadersHandle == NULL))
+		{
+			result = HTTPAPI_INVALID_ARG;
+			LogError("NULL parameter detected (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+		}
+		else
+		{
+			wchar_t* requestTypeString = NULL;
+
+			switch (requestType)
+			{
+			default:
+				break;
+
+			case HTTPAPI_REQUEST_GET:
+				requestTypeString = L"GET";
+				break;
+
+			case HTTPAPI_REQUEST_POST:
+				requestTypeString = L"POST";
+				break;
+
+			case HTTPAPI_REQUEST_PUT:
+				requestTypeString = L"PUT";
+				break;
+
+			case HTTPAPI_REQUEST_DELETE:
+				requestTypeString = L"DELETE";
+				break;
+
+			case HTTPAPI_REQUEST_PATCH:
+				requestTypeString = L"PATCH";
+				break;
+			}
+
+			if (requestTypeString == NULL)
+			{
+				result = HTTPAPI_INVALID_ARG;
+				LogError("requestTypeString was NULL (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+			}
+			else
+			{
+				const char* headers2;
+				headers2 = ConstructHeadersString(httpHeadersHandle, false);
+				if (headers2 != NULL)
+				{
+					wchar_t* relativePathTemp = AllocWide(relativePath);
+					result = HTTPAPI_OK; /*legacy code*/
+
+					if (relativePathTemp == NULL)
+					{
+						result = HTTPAPI_ALLOC_FAILED;
+						LogError("malloc failed (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+					}
+					else
+					{
+						{
+							wchar_t* headersTemp = AllocWide(headers2);
+							if (headersTemp == NULL)
+							{
+								result = HTTPAPI_STRING_PROCESSING_ERROR;
+								LogError("MultiByteToWideChar was 0. (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+							}
+							else
+							{
+								HINTERNET requestHandle = WinHttpOpenRequest(
+									handleData->ConnectionHandle,
+									requestTypeString,
+									relativePathTemp,
+									NULL,
+									WINHTTP_NO_REFERER,
+									WINHTTP_DEFAULT_ACCEPT_TYPES,
+									WINHTTP_FLAG_SECURE);
+								if (requestHandle == NULL)
+								{
+									result = HTTPAPI_OPEN_REQUEST_FAILED;
+									LogErrorWinHTTPWithGetLastErrorAsString("WinHttpOpenRequest failed (result = %s).", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+								}
+								else
+								{
+									if (WinHttpSetTimeouts(requestHandle,
+										0,                      /*_In_  int dwResolveTimeout - The initial value is zero, meaning no time-out (infinite). */
+										60000,                  /*_In_  int dwConnectTimeout, -  The initial value is 60,000 (60 seconds).*/
+										handleData->timeout,    /*_In_  int dwSendTimeout, -  The initial value is 30,000 (30 seconds).*/
+										handleData->timeout     /* int dwReceiveTimeout The initial value is 30,000 (30 seconds).*/
+									) == FALSE)
+									{
+										result = HTTPAPI_SET_TIMEOUTS_FAILED;
+										LogErrorWinHTTPWithGetLastErrorAsString("WinHttpOpenRequest failed (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+									}
+									else
+									{
+										DWORD dwSecurityFlags = 0;
+
+										if (!WinHttpSetOption(
+											requestHandle,
+											WINHTTP_OPTION_SECURITY_FLAGS,
+											&dwSecurityFlags,
+											sizeof(dwSecurityFlags)))
+										{
+											result = HTTPAPI_SET_OPTION_FAILED;
+											LogErrorWinHTTPWithGetLastErrorAsString("WinHttpSetOption failed (result = %s).", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+										}
+										else
+										{
+											if (!WinHttpSendRequest(
+												requestHandle,
+												headersTemp,
+												(DWORD)-1L, /*An unsigned long integer value that contains the length, in characters, of the additional headers. If this parameter is -1L ... */
+												(void*)content,
+												(DWORD)contentLength,
+												(DWORD)contentLength,
+												0))
+											{
+												result = HTTPAPI_SEND_REQUEST_FAILED;
+												LogErrorWinHTTPWithGetLastErrorAsString("WinHttpSendRequest: (result = %s).", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+											}
+											/*else 
+											{
+												return result;
+											}*/
+										}
+									}
+
+									//(void)WinHttpCloseHandle(requestHandle);
+									if (httpRequestHandle == NULL) {
+										(void)WinHttpCloseHandle(requestHandle);
+										result = HTTPAPI_INVALID_ARG;
+										LogError("httpRequestHandle was NULL (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+									}
+									else {
+										*httpRequestHandle = requestHandle;
+									}
+								}
+							}
+							free(headersTemp);
+						}
+						free(relativePathTemp);
+					}
+					free((void*)headers2);
+				}
+				else
+				{
+					result = HTTPAPI_ALLOC_FAILED; /*likely*/
+					LogError("ConstructHeadersString failed");
+				}
+			}
+		}
+	}
+	return result;
+}
+
+HTTPAPI_RESULT HTTPAPI_Rsponse(unsigned int* statusCode,
+	HTTP_HEADERS_HANDLE responseHeadersHandle, BUFFER_HANDLE responseContent,
+	REQUESTHANDLE httpRequestHandle, HTTPAPI_READEVENT_TYPE* ReadEvery)
+{
+	HTTPAPI_RESULT result = HTTPAPI_OK;
+
+	
+
+	if (httpRequestHandle == NULL || *httpRequestHandle == NULL)
+	{
+		result = HTTPAPI_OPEN_REQUEST_FAILED;
+		LogErrorWinHTTPWithGetLastErrorAsString("WinHttpOpenRequest failed (result = %s).", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+	}
+	else 
+	{
+		HINTERNET requestHandle = *httpRequestHandle;
+
+		DWORD responseBytesAvailable;
+
+		if (*ReadEvery == HTTPAPI_START_READ) 
+		{
+			if (!WinHttpReceiveResponse(
+				requestHandle,
+				0))
+			{
+				result = HTTPAPI_RECEIVE_RESPONSE_FAILED;
+				LogErrorWinHTTPWithGetLastErrorAsString("WinHttpReceiveResponse: (result = %s).", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+			}
+			else
+			{
+				DWORD dwStatusCode = 0;
+				DWORD dwBufferLength = sizeof(DWORD);
+
+				if (!WinHttpQueryHeaders(
+					requestHandle,
+					WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER,
+					WINHTTP_HEADER_NAME_BY_INDEX,
+					&dwStatusCode,
+					&dwBufferLength,
+					WINHTTP_NO_HEADER_INDEX))
+				{
+					result = HTTPAPI_QUERY_HEADERS_FAILED;
+					LogErrorWinHTTPWithGetLastErrorAsString("WinHttpQueryHeaders failed (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+				}
+				else
+				{
+					BUFFER_HANDLE useToReadResponse = (responseContent != NULL) ? responseContent : BUFFER_new();
+
+					if (statusCode != NULL)
+					{
+						*statusCode = dwStatusCode;
+					}
+
+					if (useToReadResponse == NULL)
+					{
+						result = HTTPAPI_ERROR;
+						LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+					}
+					else
+					{
+						*ReadEvery = HTTPAPI_GOON_READ;
+						/*from MSDN: If no data is available and the end of the file has not been reached, one of two things happens. If the session is synchronous, the request waits until data becomes available.*/
+						if (!WinHttpQueryDataAvailable(requestHandle, &responseBytesAvailable))
+						{
+							(void)WinHttpCloseHandle(requestHandle);
+							result = HTTPAPI_QUERY_DATA_AVAILABLE_FAILED;
+							LogErrorWinHTTPWithGetLastErrorAsString("WinHttpQueryDataAvailable failed (result = %s).", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+							*ReadEvery = HTTPAPI_READ_END;
+						}
+						else if (responseBytesAvailable == 0)
+						{
+							(void)WinHttpCloseHandle(requestHandle);
+							/*end of the stream, go out*/
+							result = HTTPAPI_OK;
+							*ReadEvery = HTTPAPI_READ_END;
+						}
+						else
+						{
+							if (BUFFER_enlarge(useToReadResponse, responseBytesAvailable) != 0)
+							{
+								(void)WinHttpCloseHandle(requestHandle);
+								result = HTTPAPI_ERROR;
+								LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+								*ReadEvery = HTTPAPI_READ_END;
+							}
+							else
+							{
+								/*Add the read bytes to the response buffer*/
+								size_t bufferSize;
+								const unsigned char* bufferContent;
+
+								if (BUFFER_content(useToReadResponse, &bufferContent) != 0)
+								{
+									(void)WinHttpCloseHandle(requestHandle);
+									result = HTTPAPI_ERROR;
+									LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+									*ReadEvery = HTTPAPI_READ_END;
+								}
+								else if (BUFFER_size(useToReadResponse, &bufferSize) != 0)
+								{
+									(void)WinHttpCloseHandle(requestHandle);
+									result = HTTPAPI_ERROR;
+									LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+									*ReadEvery = HTTPAPI_READ_END;
+								}
+								else
+								{
+									DWORD bytesReceived;
+									if (!WinHttpReadData(requestHandle, (LPVOID)(bufferContent + bufferSize - responseBytesAvailable), responseBytesAvailable, &bytesReceived))
+									{
+										(void)WinHttpCloseHandle(requestHandle);
+										result = HTTPAPI_READ_DATA_FAILED;
+										LogErrorWinHTTPWithGetLastErrorAsString("WinHttpReadData failed (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+										*ReadEvery = HTTPAPI_READ_END;
+									}
+									else
+									{
+										/*if for some reason bytesReceived is zero If you are using WinHttpReadData synchronously, and the return value is TRUE and the number of bytes read is zero, the transfer has been completed and there are no more bytes to read on the handle.*/
+										if (bytesReceived == 0)
+										{
+											(void)WinHttpCloseHandle(requestHandle);
+											/*end of everything, but this looks like an error still, or a non-conformance between WinHttpQueryDataAvailable and WinHttpReadData*/
+											result = HTTPAPI_READ_DATA_FAILED;
+											LogError("bytesReceived was unexpectedly zero (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+											*ReadEvery = HTTPAPI_READ_END;
+										}
+										else
+										{
+											/*all is fine, keep going*/
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+
+				if (result == HTTPAPI_OK && responseHeadersHandle != NULL)
+				{
+					result = QueryHeaders(requestHandle, responseHeadersHandle);
+				}
+			}
+		}
+		else if (*ReadEvery == HTTPAPI_GOON_READ)
+		{
+			BUFFER_HANDLE useToReadResponse = (responseContent != NULL) ? responseContent : BUFFER_new();
+
+			if (useToReadResponse == NULL)
+			{
+				result = HTTPAPI_ERROR;
+				LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+			}
+
+			*ReadEvery = HTTPAPI_GOON_READ;
+			/*from MSDN: If no data is available and the end of the file has not been reached, one of two things happens. If the session is synchronous, the request waits until data becomes available.*/
+			if (!WinHttpQueryDataAvailable(requestHandle, &responseBytesAvailable))
+			{
+				(void)WinHttpCloseHandle(requestHandle);
+				result = HTTPAPI_QUERY_DATA_AVAILABLE_FAILED;
+				LogErrorWinHTTPWithGetLastErrorAsString("WinHttpQueryDataAvailable failed (result = %s).", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+				*ReadEvery = HTTPAPI_READ_END;
+			}
+			else if (responseBytesAvailable == 0)
+			{
+				(void)WinHttpCloseHandle(requestHandle);
+				/*end of the stream, go out*/
+				result = HTTPAPI_OK;
+				*ReadEvery = HTTPAPI_READ_END;
+			}
+			else
+			{
+				if (BUFFER_enlarge(useToReadResponse, responseBytesAvailable) != 0)
+				{
+					(void)WinHttpCloseHandle(requestHandle);
+					result = HTTPAPI_ERROR;
+					LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+					*ReadEvery = HTTPAPI_READ_END;
+				}
+				else
+				{
+					/*Add the read bytes to the response buffer*/
+					size_t bufferSize;
+					const unsigned char* bufferContent;
+
+					if (BUFFER_content(useToReadResponse, &bufferContent) != 0)
+					{
+						(void)WinHttpCloseHandle(requestHandle);
+						result = HTTPAPI_ERROR;
+						LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+						*ReadEvery = HTTPAPI_READ_END;
+					}
+					else if (BUFFER_size(useToReadResponse, &bufferSize) != 0)
+					{
+						(void)WinHttpCloseHandle(requestHandle);
+						result = HTTPAPI_ERROR;
+						LogError("(result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+						*ReadEvery = HTTPAPI_READ_END;
+					}
+					else
+					{
+						DWORD bytesReceived;
+						if (!WinHttpReadData(requestHandle, (LPVOID)(bufferContent + bufferSize - responseBytesAvailable), responseBytesAvailable, &bytesReceived))
+						{
+							(void)WinHttpCloseHandle(requestHandle);
+							result = HTTPAPI_READ_DATA_FAILED;
+							LogErrorWinHTTPWithGetLastErrorAsString("WinHttpReadData failed (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+							*ReadEvery = HTTPAPI_READ_END;
+						}
+						else
+						{
+							/*if for some reason bytesReceived is zero If you are using WinHttpReadData synchronously, and the return value is TRUE and the number of bytes read is zero, the transfer has been completed and there are no more bytes to read on the handle.*/
+							if (bytesReceived == 0)
+							{
+								(void)WinHttpCloseHandle(requestHandle);
+								/*end of everything, but this looks like an error still, or a non-conformance between WinHttpQueryDataAvailable and WinHttpReadData*/
+								result = HTTPAPI_READ_DATA_FAILED;
+								LogError("bytesReceived was unexpectedly zero (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+								*ReadEvery = HTTPAPI_READ_END;
+							}
+							else
+							{
+								/*all is fine, keep going*/
+							}
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			result = HTTPAPI_INVALID_ARG;
+			LogError("HTTPAPI_READEVENT_TYPE was NULL (result = %s)", ENUM_TO_STRING(HTTPAPI_RESULT, result));
+		}
+	}
+	return result;
+}
+
 HTTPAPI_RESULT HTTPAPI_SetOption(HTTP_HANDLE handle, const char* optionName, const void* value)
 {
     HTTPAPI_RESULT result;
