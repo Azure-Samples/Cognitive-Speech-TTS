b50b723ede6c523ef8f5262a2887838ff441001c
yangguo <v-guoya@microsoft.com>
2017-04-25 18:01:03 +0800

    fix shell

--- ClientSDK/tests/normalTest/TTSClientSDK_x64.sh
+++ ClientSDK/tests/normalTest/TTSClientSDK_x64.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ ! -d "TTSClientSDK_x64" ]; then
-	mkdir TTSClientSDK_x64
+if [ ! -d "normalTest_x64" ]; then
+	mkdir normalTest_x64
 fi
 
-cd TTSClientSDK_x64
-cmake -DCPU_PLATFORM="x64" ../../../
+cd normalTest_x64
+cmake -DCPU_PLATFORM="x64" ../
 
