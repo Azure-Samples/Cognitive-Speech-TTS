4d64c7efd31ad8510a1b360e2631adcb0049577a
erhopf <erhopf@microsoft.com>
2019-01-08 09:36:36 -0800

    Simplified Python code. Sample now writes audio to file in the working directory.

--- Samples-Http/Python/TTSSample.py
+++ Samples-Http/Python/TTSSample.py
@@ -1,66 +1,81 @@
-#! /usr/bin/env python3
-
-# -*- coding: utf-8 -*-
-
-###
-#Copyright (c) Microsoft Corporation
-#All rights reserved. 
-#MIT License
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-###
-import http.client, urllib.parse, json
+'''
+After you've set your subscription key, run this application from your working
+directory with this command: python TTSSample.py
+'''
+import os, requests, time
 from xml.etree import ElementTree
 
-# Note: new unified SpeechService API key and issue token uri is per region
-# New unified SpeechService key
-# Free: https://azure.microsoft.com/en-us/try/cognitive-services/?api=speech-services
-# Paid: https://go.microsoft.com/fwlink/?LinkId=872236
-apiKey = "Your api key goes here"
+# This code is required for Python 2.7
+try: input = raw_input
+except NameError: pass
 
-params = ""
-headers = {"Ocp-Apim-Subscription-Key": apiKey}
+'''
+If you prefer, you can hardcode your subscription key as a string and remove
+the provided conditional statement. However, we do recommend using environment
+variables to secure your subscription keys. The environment variable is
+set to SPEECH_SERVICE_KEY in our sample.
 
-#AccessTokenUri = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
-AccessTokenHost = "westus.api.cognitive.microsoft.com"
-path = "/sts/v1.0/issueToken"
+For example:
+subscription_key = "Your-Key-Goes-Here"
+'''
 
-# Connect to server to get the Access Token
-print ("Connect to server to get the Access Token")
-conn = http.client.HTTPSConnection(AccessTokenHost)
-conn.request("POST", path, params, headers)
-response = conn.getresponse()
-print(response.status, response.reason)
+if 'SPEECH_SERVICE_KEY' in os.environ:
+    subscription_key = os.environ['SPEECH_SERVICE_KEY']
+else:
+    print('Environment variable for your subscription key is not set.')
+    exit()
 
-data = response.read()
-conn.close()
+class TextToSpeech(object):
+    def __init__(self, subscription_key):
+        self.subscription_key = subscription_key
+        self.tts = input("What would you like to convert to speech: ")
+        self.timestr = time.strftime("%Y%m%d-%H%M")
+        self.access_token = None
 
-accesstoken = data.decode("UTF-8")
-print ("Access Token: " + accesstoken)
+    '''
+    The TTS endpoint requires an access token. This method exchanges your
+    subscription key for an access token that is valid for ten minutes.
+    '''
+    def get_token(self):
+        fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.subscription_key
+        }
+        response = requests.post(fetch_token_url, headers=headers)
+        self.access_token = str(response.text)
 
-body = ElementTree.Element('speak', version='1.0')
-body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
-voice = ElementTree.SubElement(body, 'voice')
-voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-US')
-voice.set('{http://www.w3.org/XML/1998/namespace}gender', 'Male')
-voice.set('name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
-voice.text = 'This is a demo to call microsoft text to speech service in Python.'
+    def save_audio(self):
+        base_url = 'https://westus.tts.speech.microsoft.com/'
+        path = 'cognitiveservices/v1'
+        constructed_url = base_url + path
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+            'User-Agent': 'YOUR_RESOURCE_NAME'
+        }
+        xml_body = ElementTree.Element('speak', version='1.0')
+        xml_body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
+        voice = ElementTree.SubElement(xml_body, 'voice')
+        voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-US')
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
+        voice.text = self.tts
+        body = ElementTree.tostring(xml_body)
 
-headers = {"Content-type": "application/ssml+xml", 
-			"X-Microsoft-OutputFormat": "riff-24khz-16bit-mono-pcm",
-			"Authorization": "Bearer " + accesstoken, 
-			"X-Search-AppId": "07D3234E49CE426DAA29772419F436CA", 
-			"X-Search-ClientID": "1ECFAE91408841A480F00935DC390960", 
-			"User-Agent": "TTSForPython"}
-			
-#Connect to server to synthesize the wave
-print ("\nConnect to server to synthesize the wave")
-conn = http.client.HTTPSConnection("westus.tts.speech.microsoft.com")
-conn.request("POST", "/cognitiveservices/v1", ElementTree.tostring(body), headers)
-response = conn.getresponse()
-print(response.status, response.reason)
+        response = requests.post(constructed_url, headers=headers, data=body)
+        '''
+        If a success response is returned, then the binary audio is written
+        to file in your working directory. It is prefaced by sample and
+        includes the date.
+        '''
+        if response.status_code == 200:
+            with open('sample-' + self.timestr + '.wav', 'wb') as audio:
+                audio.write(response.content)
+                print("\nStatus code: " + str(response.status_code) + "\nYour TTS is ready for playback.\n")
+        else:
+            print("\nStatus code: " + str(response.status_code) + "\nSomething went wrong. Check your subscription key and headers.\n")
 
-data = response.read()
-conn.close()
-print("The synthesized wave length: %d" %(len(data)))
+if __name__ == "__main__":
+    app = TextToSpeech(subscription_key)
+    app.get_token()
+    app.save_audio()
