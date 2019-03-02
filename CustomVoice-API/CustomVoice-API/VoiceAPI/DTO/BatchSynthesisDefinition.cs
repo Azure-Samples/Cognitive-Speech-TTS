9b69ffdf26faaa0c8f143c8e7832c3d6fd99981e
xiaozhouz <33673047@qq.com>
2019-03-02 22:02:10 +0800

    add batch api support (#101)

---
+++ CustomVoice-API/CustomVoice-API/VoiceAPI/DTO/BatchSynthesisDefinition.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="BatchSynthesisDefinition.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+
+namespace Microsoft.SpeechServices.Cris.Http
+{
+    using System;
+
+    public sealed class BatchSynthesisDefinition
+    {
+        private BatchSynthesisDefinition(string name, string description, string locale, Uri inputTextUrl)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.InputTextUrl = inputTextUrl;
+            this.Locale = locale;
+        }
+
+        private BatchSynthesisDefinition(string name, string description, string locale, Uri inputTextUrl, ModelIdentity model)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.InputTextUrl = inputTextUrl;
+            this.Locale = locale;
+            this.Model = model;
+        }
+
+        /// <inheritdoc />
+        public string Name { get; set; }
+
+        /// <inheritdoc />
+        public string Description { get; set; }
+
+        /// <inheritdoc />
+        public Uri InputTextUrl { get; set; }
+
+        public string Locale { get; set; }
+
+        public ModelIdentity Model { get; set; }
+
+        public static BatchSynthesisDefinition Create(
+            string name,
+            string description,
+            string locale,
+            Uri inputTextUrl)
+        {
+            return new BatchSynthesisDefinition(name, description, locale, inputTextUrl, null);
+        }
+
+        public static BatchSynthesisDefinition Create(
+            string name,
+            string description,
+            string locale,
+            Uri inputTextUrl,
+            ModelIdentity model)
+        {
+            return new BatchSynthesisDefinition(name, description, locale, inputTextUrl, model);
+        }
+    }
+}
---
+++ CustomVoice-API/CustomVoice-API/VoiceAPI/DTO/Voice.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Voice.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+
+namespace Microsoft.SpeechServices.Cris.Http
+{
+    using Newtonsoft.Json;
+    using System;
+
+    public sealed class Voice
+    {
+        [JsonConstructor]
+        private Voice(Guid id, string name, string locale, string gender, bool isPublicVoice)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Gender = gender;
+            this.Locale = locale;
+            this.IsPublicVoice = isPublicVoice;
+        }
+
+        public string Name { get; set; }
+
+
+        public string Locale { get; set; }
+
+
+        public Guid Id { get; set; }
+
+
+        public string Gender { get; set; }
+
+        public bool IsPublicVoice { get; set; }
+    }
+}
