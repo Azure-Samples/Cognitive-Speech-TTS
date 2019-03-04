58967b1f4cd47e06010ef51619cf4d07db10696a
erhopf <erhopf@microsoft.com>
2019-03-04 08:48:45 -0800

    Updated Node.js sample that uses promises. Package.json has been updated with new dependencies.

--- Samples-Http/NodeJS/TTSSample.js
+++ Samples-Http/NodeJS/TTSSample.js
@@ -1,67 +1,37 @@
 // To install dependencies, run: npm install
 const xmlbuilder = require('xmlbuilder');
-const request = require('request');
+// request-promise has a dependency on request
+const rp = require('request-promise');
 const fs = require('fs');
 const readline = require('readline-sync');
 
-/* If you prefer, you can hardcode your subscription key as a string and remove
-   the provided conditional statement. However, we do recommend using environment
-   variables to secure your subscription keys.
 
-   For example:
-   subscriptionKey = "Your-Key-Goes-Here" */
-const subscriptionKey = process.env.SPEECH_SERVICE_KEY;
-if (!subscriptionKey) {
-    throw new Error('Environment variable for your subscription key is not set.')
-};
-
-// Prompts the user to input text.
-let text = readline.question('What would you like to convert to speech? ');
-
-/* This sample assumes your resource was created in the WEST US region. If you
-   are using a different region, please update the uri. */
-function textToSpeech(subscriptionKey, saveAudio) {
+// Gets an access token.
+function getAccessToken(subscriptionKey) {
     let options = {
         method: 'POST',
         uri: 'https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken',
         headers: {
             'Ocp-Apim-Subscription-Key': subscriptionKey
         }
-    };
-    // This function retrieve the access token and is passed as callback
-    // to request below.
-    function getToken(error, response, body) {
-        console.log("Getting your token...\n")
-        if (!error && response.statusCode == 200) {
-            //This is the callback to our saveAudio function.
-            // It takes a single argument, which is the returned accessToken.
-            saveAudio(body)
-        }
-        else {
-          throw new Error(error);
-        }
     }
-    request(options, getToken)
+    return rp(options);
 }
 
-/* Make sure to update User-Agent with the name of your resource.
-   You can also change the voice and output formats. See:
-   https://docs.microsoft.com/azure/cognitive-services/speech-service/language-support#text-to-speech */
-function saveAudio(accessToken) {
+// Converts text to speech using the input from readline.
+function textToSpeech(accessToken, text) {
     // Create the SSML request.
     let xml_body = xmlbuilder.create('speak')
-      .att('version', '1.0')
-      .att('xml:lang', 'en-us')
-      .ele('voice')
-      .att('xml:lang', 'en-us')
-      .att('name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
-      .txt(text)
-      .end();
+        .att('version', '1.0')
+        .att('xml:lang', 'en-us')
+        .ele('voice')
+        .att('xml:lang', 'en-us')
+        .att('name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
+        .txt(text)
+        .end();
     // Convert the XML into a string to send in the TTS request.
     let body = xml_body.toString();
 
-    /* This sample assumes your resource was created in the WEST US region. If you
-       are using a different region, please update the uri. */
     let options = {
         method: 'POST',
         baseUrl: 'https://westus.tts.speech.microsoft.com/',
@@ -74,21 +44,40 @@ function saveAudio(accessToken) {
             'Content-Type': 'application/ssml+xml'
         },
         body: body
-    };
-    /* This function makes the request to convert speech to text.
-       The speech is returned as the response. */
-    function convertText(error, response, body){
-      if (!error && response.statusCode == 200) {
-        console.log("Converting text-to-speech. Please hold...\n")
-      }
-      else {
-        throw new Error(error);
-      }
-      console.log("Your file is ready.\n")
     }
-    // Pipe the response to file.
-    request(options, convertText).pipe(fs.createWriteStream('sample.wav'));
+
+    let request = rp(options)
+        .on('response', (response) => {
+            if (response.statusCode === 200) {
+                request.pipe(fs.createWriteStream('TTSOutput.wav'));
+                console.log('\nYour file is ready.\n')
+            }
+        });
+    return request;
+
+};
+
+// Use async and await to get the token before attempting
+// to convert text to speech.
+async function main() {
+    // Reads subscription key from env variable.
+    // You can replace this with a string containing your subscription key. If
+    // you prefer not to read from an env variable.
+    // e.g. const subscriptionKey = "your_key_here";
+    const subscriptionKey = process.env.SPEECH_SERVICE_KEY;
+    if (!subscriptionKey) {
+        throw new Error('Environment variable for your subscription key is not set.')
+    };
+    // Prompts the user to input text.
+    const text = readline.question('What would you like to convert to speech? ');
+
+    try {
+        const accessToken = await getAccessToken(subscriptionKey);
+        await textToSpeech(accessToken, text);
+    } catch (err) {
+        console.log(`Something went wrong: ${err}`);
+    }
 }
 
-// Runs the sample app.
-textToSpeech(subscriptionKey, saveAudio);
+// Run the application
+main()
