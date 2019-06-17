f42e807e1cac0ead014d514209c3b55c6915c217
qingh <qingh@QINGSHENGDT>
2019-06-17 11:00:34 +0800

    Refactor the folder structure for Custom voice api samples.

---
+++ CustomVoice-API-Samples/CSharp/CustomVoice-API/VoiceAPI/DTO/ModelIdentity.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ModelIdentity.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+
+namespace Microsoft.SpeechServices.Cris.Http
+{
+    using System;
+
+    public sealed class ModelIdentity
+    {
+        public ModelIdentity(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public Guid Id { get; private set; }
+
+        public static ModelIdentity Create(Guid Id)
+        {
+            return new ModelIdentity(Id);
+        }
+    }
+}
---
+++ CustomVoice-API-Samples/CSharp/CustomVoice-API/VoiceAPI/DTO/VoiceTestDefinition.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="VoiceTestDefinition.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+
+namespace Microsoft.SpeechServices.Cris.Http
+{
+    public sealed class VoiceTestDefinition
+    {
+        public VoiceTestDefinition(
+            ModelIdentity model,
+            string text,
+            string voiceTestKind)
+        {
+            this.Model = model;
+            this.Text = text;
+            this.VoiceTestKind = voiceTestKind;
+        }
+
+        public ModelIdentity Model { get; private set; }
+
+        public string Text { get; private set; }
+
+        public string VoiceTestKind { get; private set; }
+    }
+}
